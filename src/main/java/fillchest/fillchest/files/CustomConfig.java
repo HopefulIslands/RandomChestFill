de3792570f534128b30515fa91554ac7f4446793
Overflow <77083630+KaneSeries2@users.noreply.github.com>
2021-05-04 14:18:37 -0400

    register yaml config

--- src/main/java/fillchest/fillchest/files/CustomConfig.java
+++ src/main/java/fillchest/fillchest/files/CustomConfig.java
@@ -1,4 +1,26 @@
 package fillchest.fillchest.files;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
 public class CustomConfig   {
+    private static File file;
+    private static FileConfiguration customFile;
+
+    public static void setup(){
+        file = new File(Bukkit.getServer().getPluginManager().getPlugin("RandomChestFill").getDataFolder(), "config.yaml");
+
+        if(!file.exists()){
+            try{
+                file.createNewFile()
+            }catch(IOException e){
+
+            }
+        }
+        customFile = YamlConfiguration.loadConfiguration(file);
+    }
 }
